377d7d9d2fe14939cf1e05579837a2026b6b0189
hemantic <artem.kiselev@gmail.com>
2020-05-30 02:19:17 +0300

    Tests renamed and added test for distribution thresholds

--- seller_stats/utils/stats.py
+++ seller_stats/utils/stats.py
@@ -1,18 +1,12 @@
 def get_distribution_thresholds(series):
-    thresholds = list(range(0, min([5501, int(series.max()) + 1]), 500))
+    thresholds = list(range(0, max(min([5500, int(series.max())]), 500) + 1, 500))
     labels = []
 
-    if len(thresholds) == 2:
-        labels = [thresholds[0] - thresholds[1]]
-    else:
-        for i in range(len(thresholds)):
-            if i == 0 and len(thresholds) > 0:
-                labels.append(f'<{thresholds[i + 1]}')
+    for i in range(len(thresholds)):
+        if 0 <= i < (len(thresholds) - 1):
+            labels.append(f'{thresholds[i]}-{thresholds[i + 1]}')
 
-            if 0 < i < (len(thresholds) - 2):
-                labels.append(f'{thresholds[i]}-{thresholds[i + 1]}')
-
-            if i == (len(thresholds) - 2):
-                labels.append(f'>{thresholds[i]}')
+        if i == (len(thresholds) - 1):
+            labels.append(f'>{thresholds[i]}')
 
     return thresholds, labels
---
+++ tests/test_utils_stats.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import pytest
+
+from seller_stats.utils.stats import get_distribution_thresholds
+
+
+@pytest.mark.parametrize('mock_data, mock_thresholds, mock_labels', [
+    [(1, 450, 1294, 2455, 3964, 4345), [0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000], ['0-500', '500-1000', '1000-1500', '1500-2000', '2000-2500', '2500-3000', '3000-3500', '3500-4000', '>4000']],
+    [(345, 678), [0, 500], ['0-500', '>500']],
+    [(123, 222, 359, 486), [0, 500], ['0-500', '>500']],
+])
+def test_distribution_thresholds(mock_data, mock_thresholds, mock_labels):
+    series = pd.Series(data=mock_data)
+    thresholds, labels = get_distribution_thresholds(series)
+
+    assert thresholds == mock_thresholds
+    assert labels == mock_labels
