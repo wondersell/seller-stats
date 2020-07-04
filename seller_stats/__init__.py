a0e8a2e1af120a245faf8cc967d7d139045cf0aa
hemantic <artem.kiselev@gmail.com>
2020-07-05 00:24:26 +0300

    Version bumped

--- seller_stats/__init__.py
+++ seller_stats/__init__.py
@@ -1,3 +1,3 @@
 __author__ = 'Artem Kiselev'
 __email__ = 'artem.kiselev@gmail.com'
-__version__ = '0.1.2'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='seller-stats',
-    version='0.1.2',
+    version='0.2.0',
     author='Artem Kiselev',
     author_email='artem.kiselev@gmail.com',
     description='Various statistics for marketplace sellers',
